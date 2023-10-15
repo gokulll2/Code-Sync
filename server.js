eb391db0cbcf7ad2b366df230b9e19c678f735c9
Gokul <gokulsethi69@gmail.com>
2023-10-15 18:44:36 +0530

    Some changes

--- server.js
+++ server.js
@@ -1,12 +1,19 @@
 const express = require("express");
 const app = express();
+const pathh = require('path');
 const http = require('http');
 const {Server} = require('socket.io');
 const ACTIONS = require("./src/Actions");
+const path = require("path");
 
 const server = http.createServer(app);
 const io = new Server(server);
 
+app.use(express.static('build'));
+
+app.use((req,res,next) =>{
+   res.sendFile(pathh.join(__dirname , 'build' , 'index.html'))
+})
 const userSocketMap = {};
 
 function getAllClients(roomId)
